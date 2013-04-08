ccbcd5adb21769fffbd542a8bca09cea44502f14
Lance Pollard <lancejpollard@gmail.com>
2013-04-07 22:47:40 -0700

    idea

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 
 var adapter = require('tower-adapter')
   , topology = require('tower-topology')
+  , Topology = topology('mongodb')
   , stream = require('tower-stream') // http://nodejs.org/api/stream.html
   , ReadableStream = stream.Readable;
 
@@ -29,10 +30,30 @@ exports
   .type('bitmask')
   .type('array');
 
-stream('mongodb-insert');
+/**
+ * Mongodb `insert` operation.
+ */
+
+stream('mongodb-insert')
+  .on('execute', function(node, data, fn){
+    collections[data.name].insert(data.records, fn);
+  });
+
 stream('mongodb-update');
 stream('mongodb-remove');
-stream('mongodb-find');
+
+stream('mongodb-find')
+  .on('init', function(context, data){
+    context.query = collections[data.name].find(data.conditions).stream();
+    context.query.pause();
+  })
+  .on('execute', function(context, data, fn){
+    context.query
+      .on('data', function(record){
+        // query.pause();
+        context.emit('data', record);
+      });
+  });
 
 /**
  * Execute a database query.
--- test/index.js
+++ test/index.js
@@ -2,7 +2,18 @@ var mongodb = require('..')
   , assert = require('assert');
 
 describe('mongodb adapter', function(){
-  it('should define', function(){
-    
+  it('should define', function(done){
+    var query = [
+        ['select', 'posts']
+      , ['return', 'posts']
+    ];
+
+    var topology = mongodb.execute(query);
+
+    assert('mongodb' === topology.name);
+
+    topology.on('data', function(data){
+      done();
+    });
   });
 });
